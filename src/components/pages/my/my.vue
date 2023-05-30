8c0e2b19d8bb737c8839c87021f65df5e3d78319
tangxiaomi <81195314@qq.com>
2023-05-30 17:02:45 +0800

    update

--- src/components/pages/my/my.vue
+++ src/components/pages/my/my.vue
@@ -11,15 +11,15 @@
         <div class=" relative max-w-sm mx-auto bg-white mb-10 px-6 items-center  rounded-xl flex  ">
             <img class="w-24 h-24 rounded-full shadow-lg" src="https://tangxiaomi.top/mobile/logo.svg" />
             <div class="flex flex-col py-5 pl-10">
-                <strong class="text-slate-900 text-sm font-medium dark:text-slate-200">Abby</strong>
-                <span class="text-slate-500 text-sm font-medium dark:text-slate-400">brave</span>
+                <strong class="text-slate-900 text-sm font-medium dark:text-slate-200">登录</strong>
+                <!-- <span class="text-slate-500 text-sm font-medium dark:text-slate-400">brave</span> -->
             </div>
         </div>
         <!-- <van-contact-card type="edit" tel="15807551258" name="Abby" class="mb30" /> -->
         <!-- <van-skeleton title avatar :row="3" class="mb30" /> -->
 
         <van-cell-group>
-            <van-cell title="商户账号的登录" is-link />
+            <!-- <van-cell title="商户账号的登录" is-link /> -->
             <van-cell title="消息通知" is-link />
             <van-cell title="帮助中心" is-link />
         </van-cell-group>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
-    base: '/mobile/',
+    // base: '/mobile/',
     plugins: [vue()],
     resolve: {
         alias: [
